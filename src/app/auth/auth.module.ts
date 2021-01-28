236a6c21d2e905c0aa9a1d24ed6d32a6051da211
Aleksandr Korol <korol1368@gmail.com>
2021-01-28 15:51:45 +0200

    Adding reducers for the module authorization

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,17 +4,18 @@ import {RouterModule, Routes} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 
 import {RegisterComponent} from './components/register/register.component';
+import {StoreModule} from '@ngrx/store';
+import {reducers} from './store/reducers';
 
 const routes: Routes = [
   {
     path: 'register',
-    component: RegisterComponent
-  }
+    component: RegisterComponent,
+  },
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule],
-  declarations: [RegisterComponent]
+  imports: [CommonModule, RouterModule.forChild(routes), ReactiveFormsModule, StoreModule.forFeature('auth', reducers)],
+  declarations: [RegisterComponent],
 })
-export class AuthModule {
-}
+export class AuthModule {}
