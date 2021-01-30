538699ab6e02dd35787b4a9464b299b51101956d
Aleksandr Korol <korol1368@gmail.com>
2021-01-31 01:48:58 +0200

    save data

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -12,11 +12,16 @@ import {AuthService} from './services/auth.service';
 import {BackendErrorMessagesModule} from '../shared/modules/backend-error-messages/backend-error-messages.module';
 import {PersistanceService} from '../shared/services/persistance.service';
 import {LoginEffect} from './store/effects/login.effect';
+import {LoginComponent} from './components/login/login.component';
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
 ];
 
@@ -29,7 +34,7 @@ const routes: Routes = [
     EffectsModule.forFeature([RegisterEffect, LoginEffect]),
     BackendErrorMessagesModule,
   ],
-  declarations: [RegisterComponent],
+  declarations: [RegisterComponent, LoginComponent],
   providers: [AuthService, PersistanceService],
 })
 export class AuthModule {}
