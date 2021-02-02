86ffb33bbc470b7322495b5acf283574112ecbf2
Aleksandr Korol <korol1368@gmail.com>
2021-02-02 17:10:16 +0200

    Получаем данные фида для отрисовки

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {select, Store} from '@ngrx/store';
+import {getFeedAction} from '../../store/actions/getFeed.action';
+import {Observable} from 'rxjs';
+import {GetFeedResponseInterface} from '../../types/getFeedResponse.interface';
+import {
+  errorSelector,
+  feedFeatureSelector,
+  feedSelector,
+  isLoadingSelector,
+} from '../../store/selectors';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.scss']
+  styleUrls: ['./feed.component.scss'],
 })
 export class FeedComponent implements OnInit {
+  @Input('apiUrl') apiUrlProps!: string;
 
-  constructor() { }
+  isLoading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
+  feed$!: Observable<GetFeedResponseInterface | null>;
+
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
+    this.initializeValues();
+    this.fetchData();
   }
 
+  initializeValues(): void {
+    // @ts-ignore
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    // @ts-ignore
+    this.error$ = this.store.pipe(select(errorSelector));
+    // @ts-ignore
+    this.feed$ = this.store.pipe(select(feedSelector));
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getFeedAction({url: this.apiUrlProps}));
+  }
 }
