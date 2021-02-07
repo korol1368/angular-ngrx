de65948c756fa724d14056a9e14f766131516ade
Aleksandr Korol <korol1368@gmail.com>
2021-02-07 15:54:33 +0200

    Реализуем модуль пагинации. Часть 1

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,7 +1,7 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {select, Store} from '@ngrx/store';
 import {getFeedAction} from '../../store/actions/getFeed.action';
-import {Observable} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {GetFeedResponseInterface} from '../../types/getFeedResponse.interface';
 import {
   errorSelector,
@@ -9,24 +9,47 @@ import {
   feedSelector,
   isLoadingSelector,
 } from '../../store/selectors';
+import {environment} from '../../../../../../environments/environment';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   @Input('apiUrl') apiUrlProps!: string;
 
   isLoading$!: Observable<boolean>;
   error$!: Observable<string | null>;
   feed$!: Observable<GetFeedResponseInterface | null>;
+  limit = environment.limit;
+  baseUrl!: string;
+  queryParamsSubscription!: Subscription;
+  currentPage!: number;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.initializeValues();
     this.fetchData();
+    this.initializeListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
+  }
+
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.currentPage = Number(params.page || '1');
+      }
+    );
   }
 
   initializeValues(): void {
@@ -36,6 +59,7 @@ export class FeedComponent implements OnInit {
     this.error$ = this.store.pipe(select(errorSelector));
     // @ts-ignore
     this.feed$ = this.store.pipe(select(feedSelector));
+    this.baseUrl = this.router.url.split('?')[0];
   }
 
   fetchData(): void {
